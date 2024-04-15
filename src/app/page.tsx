cab419c1f4cb97cc1aebce82fcfbdde87b0345c6
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-04-15 15:01:32 -0400

    create post schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,36 @@
+import { z } from "zod";
 import { db } from "~/server/db";
+import { insertPostSchema } from "~/server/db/schema";
 
 const mockUrls = [
   "https://utfs.io/f/ece8a4a8-59d1-4e8b-8403-99fc0f6c07fd-er7zyk.png",
   "https://utfs.io/f/4a48f3e8-1388-4147-b2fa-311c898081ac-55m2zr.JPG",
 ];
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export const dynamic = "force-dynamic";
+
+function Post(props: { post: z.infer<typeof insertPostSchema> }) {
+  const { post } = props;
+  return (
+    <div>
+      <h2>{post.text}</h2>
+      {post.imageUrl !== "" ? (
+        <img className="w-48" src={post.imageUrl || ""} />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
 
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
-  console.log(posts);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
         {posts.map((post) => {
-          return <div key={post.id}>{post.name}</div>;
-        })}
-        {mockImages.map((image) => {
-          return (
-            <div key={image.id} className="w-48">
-              <img src={image.url} />
-            </div>
-          );
+          return <Post post={post} />;
         })}
       </div>
     </main>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,16 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -18,17 +20,47 @@ import {
  */
 export const createTable = pgTableCreator((name) => `trashv2_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+// export const posts = createTable(
+//   "post",
+//   {
+//     id: serial("id").primaryKey(),
+//     name: varchar("name", { length: 256 }),
+//     createdAt: timestamp("created_at")
+//       .default(sql`CURRENT_TIMESTAMP`)
+//       .notNull(),
+//     updatedAt: timestamp("updatedAt"),
+//   },
+//   (example) => ({
+//     nameIndex: index("name_idx").on(example.name),
+//   }),
+// );
+
+export const users = createTable("users", {
+  id: varchar("id").primaryKey(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  posts: many(posts),
+}));
+
+export const posts = createTable("post", {
+  id: serial("id").primaryKey(),
+  authorId: varchar("author_id")
+    .notNull()
+    .references(() => users.id),
+  text: varchar("text", { length: 280 }),
+  imageUrl: varchar("image_url"),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt"),
+});
+
+export const postsRelations = relations(posts, ({ one }) => ({
+  author: one(users, {
+    fields: [posts.authorId],
+    references: [users.id],
+  }),
+}));
+
+export const insertPostSchema = createInsertSchema(posts);
